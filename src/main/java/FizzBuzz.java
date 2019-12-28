a3c072a733a117a8d23ade10c88f8d1bbd25abaa
yixianyu73 <yixianyu73@126.com>
2019-12-28 13:40:51 +0800

    新增需求

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,20 +1,21 @@
 public class FizzBuzz {
     private static final String FIZZBUZZ = "FizzBuzz";
+    private static final String FIZZWHIZZ = "FizzWhizz";
+    private static final String BUZZWHIZZ = "BuzzWhizz";
+    private static final String FIZZBUZZWHIZZ = "FizzBuzzWhizz";
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
-    private static final String ERROR = "Error";
+    private static final String WHIZZ = "Whizz";
 
-    public static String fizzBuzz(int inputNum, int firstNum, int secondNum) {
+    public static String fizzBuzz(int inputNum, int firstNum, int secondNum, int thirtyNum) {
         String result = String.valueOf(inputNum);
 
         if (inputNum <= 0) {
-            result = String.valueOf(inputNum);
-            return result;
+            throw new IllegalArgumentException("Invalid Input");
         }
 
         if (firstNum <= 0 || secondNum <= 0) {
-            result = ERROR;
-            return result;
+            throw new IllegalArgumentException("Invalid Input");
         }
         if (isFizz(inputNum, firstNum)) {
             result = FIZZ;
@@ -22,14 +23,38 @@ public class FizzBuzz {
         if (isBuzz(inputNum, secondNum)) {
             result = BUZZ;
         }
+        if (isFizz(inputNum, thirtyNum)) {
+            result = WHIZZ;
+        }
         if (isFizzBuzz(inputNum, firstNum, secondNum)) {
             result = FIZZBUZZ;
+        }
+        if (isFizzWhizz(inputNum, firstNum, thirtyNum)) {
+            result = FIZZWHIZZ;
+        }
+        if (isBuzzWhizz(inputNum, secondNum, thirtyNum)) {
+            result = BUZZWHIZZ;
+        }
+        if (isFizzBuzzWhizz(inputNum, firstNum, secondNum, thirtyNum)) {
+            result = FIZZBUZZWHIZZ;
         }
         return result;
     }
 
+    private static boolean isFizzBuzzWhizz(int inputNum, int firstNum, int secondNum, int thirtyNum ) {
+        return isFizz(inputNum, firstNum) && isBuzz(inputNum, secondNum) && isWhizz(inputNum, thirtyNum);
+    }
+
     private static boolean isFizzBuzz(int inputNum, int firstNum, int secondNum) {
         return isFizz(inputNum, firstNum) && isBuzz(inputNum, secondNum);
+    }
+
+    private static boolean isFizzWhizz(int inputNum, int firstNum, int thirtyNum) {
+        return isFizz(inputNum, firstNum) && isWhizz(inputNum, thirtyNum);
+    }
+
+    private static boolean isBuzzWhizz(int inputNum, int secondNum, int thirtyNum) {
+        return isBuzz(inputNum, secondNum) && isWhizz(inputNum, thirtyNum);
     }
 
     private  static boolean isFizz(int inputNum, int firstNum) {
@@ -40,11 +65,15 @@ public class FizzBuzz {
         return count(inputNum, secondNum);
     }
 
+    private static boolean isWhizz(int inputNum, int thirtyNum) {
+        return count(inputNum, thirtyNum);
+    }
+
     private static boolean count(int inputNum, int num) {
         if (num == 0) {
             return false;
         } else {
-            return (inputNum % num == 0 || String.valueOf(inputNum).contains(String.valueOf(num)));
+            return (inputNum % num == 0);
         }
     }
 
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,36 +1,69 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void fizzBuzzReturnFizz() {
-        assertEquals("Fizz", FizzBuzz.fizzBuzz(3, 3, 5));
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(3, 3, 5, 7));
     }
 
     @Test
-    public void fizzBuzzReturnFizzbuzz() {
-        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(15, 3, 5));
+    public void fizzBuzzReturnFizzBuzz() {
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(15, 3, 5, 7));
     }
 
     @Test
     public void fizzBuzzReturnBuzz() {
-        assertEquals("Buzz", FizzBuzz.fizzBuzz(5, 3, 5));
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(5, 3, 5,7));
     }
 
     @Test
     public void fizzBuzzReturnNormal() {
-        assertEquals("1", FizzBuzz.fizzBuzz(1, 3, 5));
+        assertEquals("1", FizzBuzz.fizzBuzz(1, 3, 5,7));
+    }
+
+    @Test
+    public void fizzBuzzReturnWhizz() {
+        assertEquals("Whizz", FizzBuzz.fizzBuzz(7, 3, 5,7));
+    }
+
+    @Test
+    public void fizzBuzzReturnFizzWhizz() {
+        assertEquals("FizzWhizz", FizzBuzz.fizzBuzz(21, 3, 5, 7));
+    }
+
+    @Test
+    public void fizzBuzzReturnBuzzWhizz() {
+        assertEquals("BuzzWhizz", FizzBuzz.fizzBuzz(35, 3, 5, 7));
+    }
+
+    @Test
+    public void fizzBuzzReturnFizzBuzzWhizz() {
+        assertEquals("FizzBuzzWhizz", FizzBuzz.fizzBuzz(105, 3, 5, 7));
     }
 
     @Test
     public void fizzBuzzInputZero() {
-        assertEquals("0", FizzBuzz.fizzBuzz(0, 1, 5));
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Invalid Input");
+        FizzBuzz.fizzBuzz(0, 1, 5,7);
+//        assertEquals("0", FizzBuzz.fizzBuzz(0, 1, 5,7));
+
     }
 
     @Test
     public void fizzBuzzFirstNumZero() {
-        assertEquals("Error", FizzBuzz.fizzBuzz(1, 0, 5));
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Invalid Input");
+        FizzBuzz.fizzBuzz(1, 0, 5, 7);
+//        assertEquals("Error", FizzBuzz.fizzBuzz(1, 0, 5, 7));
     }
+
+
 }
